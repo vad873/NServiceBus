983904f162bca83fa44424ec9f5855557cd64bf5
Cameron MacFarland <distantcam@gmail.com>
2014-08-12 18:07:40 +0800

    Failing tests for #2301

---
+++ src/NServiceBus.AcceptanceTests/PubSub/When_publishing_an_event_with_multiple_inheritance.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+
+namespace NServiceBus.AcceptanceTests.PubSub
+{
+    using System;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+
+    public class When_publishing_an_event_with_multiple_inheritance
+    {
+        [Test]
+        public void All_message_handlers_should_be_invoked()
+        {
+            var context = new Context();
+
+            Scenario.Define(context)
+                .WithEndpoint<Publisher>(b =>
+                    b.Given(bus => bus.Send(new Publisher.MyEvent())))
+                .WithEndpoint<Subscriber>()
+                .Done(c => c.FirstEventHandled && c.SecondEventHandled)
+                .Run(TimeSpan.FromSeconds(15));
+
+            Assert.True(context.FirstEventHandled, "The first message failed.");
+            Assert.True(context.SecondEventHandled, "The second message failed.");
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool FirstEventHandled { get; set; }
+
+            public bool SecondEventHandled { get; set; }
+        }
+
+        public class Publisher : EndpointConfigurationBuilder
+        {
+            public class MyEvent : IMyFirstEvent, IMySecondEvent
+            {
+            }
+
+            public Publisher()
+            {
+                EndpointSetup<DefaultServer>()
+                    .AddMapping<MyEvent>(typeof(Subscriber));
+            }
+        }
+
+        public class Subscriber : EndpointConfigurationBuilder
+        {
+            public Subscriber()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class MyFirstHandler : IHandleMessages<IMyFirstEvent>
+            {
+                public Context Context { get; set; }
+
+                public void Handle(IMyFirstEvent message)
+                {
+                    Context.FirstEventHandled = true;
+                }
+            }
+
+            public class MySecondHandler : IHandleMessages<IMySecondEvent>
+            {
+                public Context Context { get; set; }
+
+                public void Handle(IMySecondEvent message)
+                {
+                    Context.SecondEventHandled = true;
+                }
+            }
+        }
+
+        public interface IMyFirstEvent : IMessage // Using message to avoid checks for sending events
+        {
+        }
+
+        public interface IMySecondEvent : IMessage
+        {
+        }
+    }
+}
---
+++ src/NServiceBus.Core.Tests/Deserializing/ExtractLogicalMessageBehaviorTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace NServiceBus.Core.Tests.Deserializing
+{
+    using System.Linq;
+    using MessageInterfaces.MessageMapper.Reflection;
+    using NServiceBus.Pipeline.Contexts;
+    using Serialization;
+    using Unicast;
+    using Unicast.Messages;
+
+    public class ExtractLogicalMessageBehaviorTests
+    {
+        [Test]
+        public void Test()
+        {
+            var mapper = new MessageMapper();
+            mapper.Initialize(new[] { typeof(IMyFirstEvent), typeof(IMySecondEvent) });
+
+            var metadataRegistry = new MessageMetadataRegistry();
+            metadataRegistry.RegisterMessageType(typeof(IMyFirstEvent));
+            metadataRegistry.RegisterMessageType(typeof(IMySecondEvent));
+
+            var behavior = new ExtractLogicalMessagesBehavior();
+            behavior.UnicastBus = new UnicastBus();
+            behavior.MessageSerializer = new NopSerializer(mapper);
+            behavior.LogicalMessageFactory = new LogicalMessageFactory();
+            behavior.LogicalMessageFactory.MessageMapper = mapper;
+            behavior.LogicalMessageFactory.MessageMetadataRegistry = metadataRegistry;
+            behavior.MessageMetadataRegistry = metadataRegistry;
+
+            var transportMessage = new TransportMessage();
+            transportMessage.Headers.Add(Headers.EnclosedMessageTypes, string.Join(";", typeof(MyEvent), typeof(IMyFirstEvent), typeof(IMySecondEvent)));
+            transportMessage.Body = new byte[1];
+
+            var context = new ReceivePhysicalMessageContext(null, transportMessage, false);
+
+            behavior.Invoke(context, () => { });
+
+            Assert.IsInstanceOf<IMyFirstEvent>(context.LogicalMessages.Single().Instance);
+            Assert.IsInstanceOf<IMySecondEvent>(context.LogicalMessages.Single().Instance);
+        }
+
+        public interface IMyFirstEvent : IMessage // Using message to avoid checks for sending events
+        {
+        }
+
+        public interface IMySecondEvent : IMessage
+        {
+        }
+
+        public class MyEvent : IMyFirstEvent, IMySecondEvent
+        {
+        }
+
+        public class NopSerializer : IMessageSerializer
+        {
+            private readonly MessageMapper mapper;
+
+            public NopSerializer(MessageMapper mapper)
+            {
+                this.mapper = mapper;
+            }
+
+            public void Serialize(object[] messages, Stream stream)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object[] Deserialize(Stream stream, IList<Type> messageTypes = null)
+            {
+                if (messageTypes == null)
+                    return new object[0];
+
+                var result = new object[messageTypes.Count];
+
+                var i = 0;
+                foreach (var messageType in messageTypes)
+                {
+                    result[i++] = mapper.CreateInstance(messageType);
+                }
+
+                return result;
+            }
+
+            public string ContentType
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+    }
+}
